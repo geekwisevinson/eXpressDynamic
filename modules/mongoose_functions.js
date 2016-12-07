182f80d723c99b741626c60f923460d0e0763300
Vinson Fernandez <geekwise.vinson.fernandez@gmail.com>
2016-12-07 14:59:52 -0800

    mongoose

--- modules/mongoose_functions.js
+++ modules/mongoose_functions.js
@@ -1,53 +1,40 @@
-module.exports = ()=>{
+/*
+ db functions
+ mongoose should be imported here
+ */
 
-	Model_Save_New = (Mongoose_Model,data_to_save)=>{
-			let mongoose_model = new Mongoose_Model(data_to_save);
-			return mongoose_model.save();
-	};
+/*
+ all functions should follow this standard with params
+ first parameter should be Model
+ second should be the query object containing the identifying properties
+ last should be the data being saved
+ */
 
-	Model_Get_All = (Mongoose_Model)=>{
+module.exports = {
 
+	Save_New : (Model,data)=>{
+		let model = new Model(data);
+		return model.save();
+	},
 
-			return Mongoose_Model.find({});
+	Get_All : (Model)=>{
+		return Model.find({});
+	},
 
+	Get_Some : (Model,query)=>{
+		return Model.find(query);
+	},
 
-	};
-	//
-	// Model_Get_Some = (Mongoose_Model,query_object,req,res)=>{
-	// 	Mongoose_Model.find(query_object, function(err, array_of_objects) {
-	// 		if (err)
-	// 			res.json({message:err,success:false});
-	// 		if(array_of_objects)
-	// 			res.json({data:array_of_objects,success:true});
-	// 	});
-	// };
-	//
-	// Model_Get_One = (Mongoose_Model,query_object,req,res)=>{
-	// 	Mongoose_Model.findOne(query_object, function(err, object) {
-	// 		if (err) console.log(err);
-	// 		if(object){
-	// 			object.remove(function(err) {
-	// 				if (err)
-	// 					res.json({message:err,success:false});
-	//
-	// 					res.json({data:query_object,success:true});
-	// 			});
-	// 		}
-	// 	});
-	// };
-	//
-	// Model_Delete_One = (Mongoose_Model,query_object,req,res)=>{
-	// 	Mongoose_Model.findOne(query_object, function(err, object) {
-	// 		if (err)
-	// 			res.json({message:err,success:false});
-	// 		if(object)
-	// 			res.json({data:object,success:true});
-	// 	});
-	// };
-	//
-	Model_Update_One = (Mongoose_Model,req,query_object)=>{
+	Get_One : (Model,query)=>{
+		return Model.findOne(query);
+	},
 
-		return Mongoose_Model.findOneAndUpdate({_id:req.params._id},query_object);
-	};
+	Delete_One : (Model,query)=>{
+		return Model.remove(query);
+	},
+
+	Update_One : (Model,query,data)=>{
+		return Model.findOneAndUpdate(query,data);
+	}
 
 };
